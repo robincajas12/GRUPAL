b2cd37f457d314d1e4321450b7b4082fe4c464dc
robincajas12 <robincajas2020@gmail.com>
2025-02-10 16:44:01 -0500

    xd

--- src/main/java/application/service/UserImporter.java
+++ src/main/java/application/service/UserImporter.java
@@ -5,28 +5,5 @@ import java.io.IOException;
 import java.util.List;
 import application.database.UsuarioAd;
 public class UserImporter {
-    private  UserService userService;
-    private  UsuarioAd userAd;
 
-    public UserImporter() {
-        this.userService = new UserService();
-        this.userAd = new UsuarioAd();
-    }
-
-    public void importUsers() {
-        try {
-            List<User> users = userService.getAllUsers();
-            for (User user : users) {
-                if (userAd.obtenerPorId(user.id()) == null) { 
-                    userAd.crear(user);
-                } else {
-                    userAd.actualizar(user);
-                }
-            }
-            System.out.println("Importación de usuarios completada.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Error al importar usuarios desde la API.");
-        }
-    }
 }
